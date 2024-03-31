8a2c72e2b9fbdbfaa72393aaa09f8086007d5276
Stanislav <spotes@yandex.ru>
2024-03-31 16:35:29 +0300

    Task2

---
+++ task1/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[10]; // Создаем массив из 10 элементов
+
+        // Заполняем массив случайными трехзначными числами
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = RandomNumber();
+        }
+
+        // Подсчет количества четных чисел в массиве
+        int evenCount = 0;
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+    }
+
+    // Метод для генерации случайного трехзначного числа
+    static int RandomNumber()
+    {
+        return new Random().Next(100, 1000); // Генерация числа от 100 до 999
+    }
+}
