d9b5969708f18f9dc15f0738a0c43a7bc666501c
Stanislav <spotes@yandex.ru>
2024-03-31 16:52:09 +0300

    Task3

---
+++ task1/task2/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 3: Напишите программу, которая перевернёт одномерный массив
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = {1, 2, 3, 4, 5}; // Исходный массив
+        Array.Reverse(array); // Переворачиваем массив
+        
+        Console.WriteLine("Перевернутый массив: ");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
